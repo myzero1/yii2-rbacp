5ba9ab40122f26d0225552332f4b35edb06c4151
qinxianwu <917595913@qq.com>
2018-01-25 00:14:52 +0800

    fix 403

--- src/views/default/rbacp403.php
+++ src/views/default/rbacp403.php
@@ -1,3 +1,7 @@
+<?php
+    $this->title = '403没有访问权限（403 forbidden）'
+?>
+
 <div class="page-error-wrap">
     <div class="page-error-conent">
         <div class="page-error-left">
@@ -6,11 +10,16 @@
             </div>
         </div>
         <div class="page-error-right">
-            <div class="page-error-title">
-                403
-            </div>
-            <div class="page-error-subtitle">
-                FORBIDDEN
+            <div class="page-error-right-conent">
+                <div class="page-error-title">
+                    403
+                </div>
+                <div class="page-error-subtitle">
+                    FORBIDDEN
+                </div>
+                <div class="page-error-subtitle2">
+                    没有访问权限
+                </div>
             </div>
         </div>
     </div>
